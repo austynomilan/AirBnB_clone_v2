955daa44c2b56fe723af13fbee3be005c539235a
Augustine <austynomilano77@gmail.com>
2024-01-22 08:03:45 -0800

    [code]:function to switch between starge engines, db or filr

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
+if storage_type.lower() == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
