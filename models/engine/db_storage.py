e437fb70cf535f3dda790742a5d1dc67172c9470
Augustine <austynomilano77@gmail.com>
2024-01-25 04:02:28 -0800

    [debug]: deubbed db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,34 +14,21 @@ from models.user import User
 
 class DBStorage:
     """Data storage engine"""
+    __engine = None
+    __session = None
+
     def __init__(self):
-        self.__engine = None
-	self.__session = None
-	self.create_engine()
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB'),
+            pool_pre_ping=True
+	))
+   
 
-    def create_engine(self):
-        """Create the engine and link it to the MySQL database."""
-        db_user = os.environ.get('HBNB_MYSQL_USER', 'hbnb_dev')
-        db_password = os.environ.get('HBNB_MYSQL_PWD', 'default pwd')
-        db_host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
-        db_name = os.environ.get('HBNB_MYSQL_DB', 'hbnb_dev_db')
-        env = os.environ.get('HBNB_ENV', 'production')
-
-	engine_str = f'mysql+mysqldb://{db_user}:{db_password}@{db_host}/{db_name}'
-        self.__engine = create_engine(engine_str, pool_pre_ping=True)
-
-        if env == 'test':
-            metadata = MetaData(self.__engine)
-            metadata.reflect()
-            metadata.drop_all()
-
-    def init_session(self):
-        """Initiate a session to interact with the database."""
-        if self.__engine is not None:
-            Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-	    self.__session = scoped_session(Session)
-        else:
-            print("Error: Engine not created. Call create_engine() first.")
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self._engine)
 
     def new(self, obj):
         """Add the object to the current database session."""
@@ -62,27 +49,44 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self._session = Session
+        self._session = Session()
         
         State.cities = relationship("City", cascade="all, delete-orphan", backref="state")
 
     def all(self, cls=None):
         """Query all objects based on the class name or all objects if cls=None."""
-        from models.base_model import Base
+        classes = {
+            "City": City,
+            "State": State,
+            "User": User,
+            "Place": Place,
+            "Review": Review,
+            "Amenity": Amenity
+        }
        
-        result_dict = {}
+        result = {}
+        query_rows = []
+
         if cls:
-            objects = self.__session.query(cls).all()
+            """Query for all objects belonging to cls"""
+            if type(cls) is str:
+                cls = eval(cls)
+            query_rows = self.__session.query(cls)
+            for obj in query_rows:
+                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                result[key] = obj
+            return result
+
         else:
-            all_classes = [User, State, City, Amenity, Place, Review]
-            objects = []
-            for model_class in all_classes:
-                objects.extend(self.__session.query(model_class).all())
-
-        for obj in objects:
-            key = f"{obj.__class__.__name__}.{obj.id}"
-            result_dict[key] = obj
-        return result_dict
-
-
- 
+            """Query for all types of object"""
+            for name, value in classes.items():
+                query_rows = self.__session.query(value)
+                for obj in query_rows:
+                    key = '{}.{}'.format(name, obj.id)
+                    result[key] = obj
+            return result
+	    
+    def close(self):
+        """Because SQLAlchemy doesn't reload his `Session`
+        when it's time to insert new data, we force it to!"""
+        self.__session.close()
