b50d9080e9a17fe45487fdf352ab4a11bee10e90
Augustine <austynomilano77@gmail.com>
2024-02-04 04:27:43 -0800

    [debug]: code base debugged

--- models/city.py
+++ models/city.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-
-"""
-A module that defines the ORM class for City table
-"""
-from os import getenv
+"""This is the city class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, ForeignKey, String
+from models.place import Place
 
 
 class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
     """
-    The city class, contains state ID and name
-    """
-    __tablename__ = 'cities'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship(
-            'Place', backref='cities', cascade='all, delete, delete-orphan')
-    else:
-        name = ''
-        state_id = ''
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
--- models/state.py
+++ models/state.py
@@ -1,22 +1,35 @@
+#!/usr/bin/python3
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
+
+
 class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
     """
-    Defines attributes for State table
-    """
-    __tablename__ = 'states'
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete')
-    else:
-        name = ''
-
-        @property
-        def cities(self):
-            """
-            Returns (list): List of City instances
-            with state_id equals to the current State.id
-            """
-            from models.city import City
-            from models import storage
-            city_list = list(storage.all(City).values())
-            return [city for city in city_list if city.state_id == self.id]
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
