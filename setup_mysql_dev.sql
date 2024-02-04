045036a8678cd3f598fcf092e4d2bffc02142907
Augustine <austynomilano77@gmail.com>
2024-02-04 08:03:47 -0800

    [code]: debug

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,6 @@
 -- Grant privileges om database 
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* To 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,7 @@
 -- creates the MySQL server Database hbnb_test_db and user hbnb_test
 -- Grant privileges om database 
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* To 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
