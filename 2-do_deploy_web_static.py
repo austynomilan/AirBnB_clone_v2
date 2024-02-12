fd15bddae736d432e49ebca01891abc187ec357c
Tommytinny <tommyteenboi@gmail.com>
2024-02-12 12:51:33 -0500

    fixed some little bugs

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,12 +14,12 @@ def do_deploy(archive_path):
         return False
     try:
         file_name = archive_path.split("/")[-1]
-        file_no_ext = file_name.split(".").[0]
+        file_no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run("mkdir -p {}{}/".format(path, file_no_ext))
         run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, file_no_ext))
-        run("rm /tmp/()".format(file_name))
+        run("rm /tmp/{}".format(file_name))
         run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_no_ext))
         run("rm -rf {}{}/web_static".format(path, file_no_ext))
         run("rm -rf /data/web_static/current")
